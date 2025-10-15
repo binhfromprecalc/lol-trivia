1d05a2814201322f2a902600ff76bf026821b82a
Binh Dang <btd514@gmail.com>
2025-10-14 21:16:15 -0700

    reminder to implement player caching functionality to reduce lookup timing

--- pages/api/account.ts
+++ pages/api/account.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getAccountByRiotId } from '@lib/riot';
+import { prisma } from "@lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { gameName, tagLine } = req.query;
@@ -10,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const account = await getAccountByRiotId(gameName, tagLine);
-    res.status(200).json(account);
+    res.status(200).json({account, cached: true});
   } catch (error: any) {
     console.error(error);
     res.status(500).json({ error: 'Failed to fetch account data' });
